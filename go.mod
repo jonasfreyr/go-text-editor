006718fe824f81d5affa12a68fc030ff53166f5f
Jónas <jonasf@ccpgames.com>
2024-02-09 14:34:46 +0000

    feat: ctrl arrows and copy and paste

--- go.mod
+++ go.mod
@@ -3,3 +3,5 @@ module github.com/jonasfreyr/playground
 go 1.18
 
 require github.com/rthornton128/goncurses v0.0.0-20231014161942-82671379df88
+
+require github.com/atotto/clipboard v0.1.4
--- main.go
+++ main.go
@@ -2,10 +2,12 @@ package main
 
 import (
 	"fmt"
+	"github.com/atotto/clipboard"
 	"log"
 	"os"
 	"strings"
 
+	"github.com/jonasfreyr/playground/utils"
 	gc "github.com/rthornton128/goncurses"
 )
 
@@ -81,6 +83,26 @@ func (e *Editor) Init() {
 func (e *Editor) End() {
 	gc.End()
 }
+
+// This needs testing when it comes to multi line deletes
+func (e *Editor) deleteLines(y, num int) {
+	if len(e.text) <= 0 {
+		return
+	} else if len(e.text) == 1 {
+		e.text[y] = ""
+	} else {
+		e.text = append(e.text[:y], e.text[y+num:]...)
+	}
+}
+
+func (e *Editor) clampXToLineOrLengthIndex() {
+	if e.currLengthIndex > len(e.text[e.y]) {
+		e.x = len(e.text[e.y])
+	} else {
+		e.x = e.currLengthIndex
+	}
+}
+
 func (e *Editor) Load(filePath string) error {
 	lines, err := os.ReadFile(filePath)
 	if err != nil {
@@ -118,32 +140,60 @@ func (e *Editor) Run() error {
 
 		updateLengthIndex := true
 
+		log.Println("This thing:", key)
+
 		switch key {
 		case gc.KEY_ESC:
 			return nil
+		case 561: // CTRL + Right
+			str := e.text[e.y][e.x:]
+			i := strings.Index(str, " ")
+			if i == -1 {
+				e.x = len(e.text[e.y])
+			} else if i == 0 {
+				e.x++
+			} else {
+				e.x = e.x + i
+			}
+		case 526: // CTRL + Down
+		case 546: // CTRL + Left
+			str := e.text[e.y][:e.x]
+			i := strings.LastIndex(str, " ")
+			if i == -1 {
+				e.x = 0
+			} else if i == len(str)-1 {
+				e.x--
+			} else {
+				e.x = i + 1
+			}
+		case 567: // CTRL + Up
+		case 4: // CTRL + D
+			e.deleteLines(e.y, 1)
+			e.y--
+			e.y = utils.Min(utils.Max(len(e.text)-1, 0), utils.Max(e.y, 0))
+			e.clampXToLineOrLengthIndex()
+		case 24: // CTRL + X
+			text := e.text[e.y] + "\n"
+			err := clipboard.WriteAll(text)
+			e.deleteLines(e.y, 1)
+			if err != nil {
+				panic(err)
+			}
 		case gc.KEY_DOWN:
 			if e.y >= len(e.text)-1 {
 				continue
 			}
 
-			if e.currLengthIndex > len(e.text[e.y+1]) {
-				e.x = len(e.text[e.y+1])
-			} else {
-				e.x = e.currLengthIndex
-			}
 			e.y++
+			e.clampXToLineOrLengthIndex()
 			updateLengthIndex = false
 		case gc.KEY_UP:
 			if e.y <= 0 {
 				continue
 			}
 
-			if e.currLengthIndex > len(e.text[e.y-1]) {
-				e.x = len(e.text[e.y-1])
-			} else {
-				e.x = e.currLengthIndex
-			}
 			e.y--
+			e.clampXToLineOrLengthIndex()
 			updateLengthIndex = false
 		case gc.KEY_LEFT:
 			if e.x <= 0 {
@@ -219,6 +269,7 @@ func (e *Editor) Run() error {
 			e.currLengthIndex = e.x
 		}
 
+		e.y = utils.Min(utils.Max(len(e.text)-1, 0), e.y)
 		e.draw()
 	}
 }
@@ -243,7 +294,11 @@ func main() {
 	e.Init()
 	defer e.End()
 
-	f, _ := os.OpenFile("info.log", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
+	f, err := os.Create("info.txt")
+	if err != nil {
+		panic(err)
+	}
+
 	log.SetOutput(f)
 	defer f.Close()
 
@@ -251,7 +306,7 @@ func main() {
 		_ = e.Load(path)
 	}
 
-	err := e.Run()
+	err = e.Run()
 	if err != nil {
 		panic(err)
 	}
